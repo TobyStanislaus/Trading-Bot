359c08b6d744b0ecedfe5569dbfbf082c247dbee
Tobyisepik <116378333+Tobyisepik@users.noreply.github.com>
2024-07-11 21:27:15 +0100

    a

---
+++ tradingbot.py
@@ -0,0 +1,14 @@
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+from datetime import datetime
+
+API_KEY=''
+API_SECRET=''
+BASE_URL=''
+ALPACA_CREDS={
+  'API_KEY':API_KEY,
+  'API_SECRET':API_SECRET,
+  'API_KEY':True,
+}
+
